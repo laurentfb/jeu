10889d20ae1af11413d7906539a9d7d92f094929
Laurent Finet-Baron <lfinetbaron@gmail.com>
2019-10-28 16:53:37 +0100

    Test-xml : ajout test section unique par paragraphe

--- test-xml.py
+++ test-xml.py
@@ -11,6 +11,7 @@ StringIO = io.BytesIO if PY2 else io.StringIO
 characters_to_ignore = "&"
 
 def XmlValidate(xml_to_validate, line_number, csv_col_number):
+    # Test #1 : verification validité xml
     clean_xml_to_validate = ''.join(c for c in xml_to_validate if c not in characters_to_ignore)
     try:
         doc = etree.fromstring("<xml>" + clean_xml_to_validate + "</xml>")
@@ -20,6 +21,14 @@ def XmlValidate(xml_to_validate, line_number, csv_col_number):
     except:
         print("  - #" + str(line_number) + " Error (formatage xml colonne "+str(csv_col_number)+" : " + xml_to_validate)
         return False
+
+    # Test #2 : Sur chaque ligne, une seule taille de police
+    # Dans chaque paragraphe, on doit avoir une seule dimension (ie une seule section)
+    # ie <paragraph><section>sdsdsd</section></paragraph>
+    for paragraph in doc.findall('paragraph'):
+        if len(paragraph.findall('section'))>1:
+            print("  - #" + str(line_number) + " Erreur une seule section autorisée par paragraphe (colonne "+str(csv_col_number)+")\n"+xml_to_validate)
+
     return True
 
 # ------------ MAIN ---------------------
